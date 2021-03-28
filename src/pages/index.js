dc044a046c4959e42e214dc9a068ee8bc01c2ce4
Mladja89 <goamladja2006@gmail.com>
2021-03-28 07:21:02 +0200

    navigator3

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default () => {
   const rootMarginValues = "2000px 0px -200px 0px";
   const [scrollY, setScrollY] = useState(0);
   const [mobile, setMobile] = useState(false)
-  if (window && window.navigator && window.navigator.userAgent){
+  if (typeof window !== 'undefined'){
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
     setMobile(true);
